5d4b8cd4f69dc99ca2230cb3fb20f1fb5e50a12e
Antonio FIlipov <tonkatafilipov@gmail.com>
2015-04-03 12:16:55 +0300

    add .toString()

--- week3/Task3_Prototypes/stringPrototypes.js
+++ week3/Task3_Prototypes/stringPrototypes.js
@@ -26,9 +26,38 @@ String.prototype.words = function(){
     }
 
 //TODO .format()
+String.prototype.format = function(dict) {
+  var result = this;
+
+  if(typeof(dict) === "object") {
+    Object.keys(dict).forEach(function(key) {
+      result = result.replace("{" + key + "}", dict[key]);
+    });
+    return result;
+  }
+
+  var args = [];
+  var n = arguments.length;
+  var i = 0;
+
+  for(i; i < n; i+=1) {
+    args.push(arguments[i]);
+  }
+
+  var result = this;
+
+  args.forEach(function(arg) {
+    result = result.replace("{}", arg);
+  });
+
+  return result;
+}
 
 console.log("javaScript".capitalize());
 console.log("".isBlank());
 console.log(" ".isBlank());
 console.log(" sad".isBlank());
 console.log("This is    a   very   clever   sentence!".words());
+
+var result = "Hi, my name is {}. Nice to meet you {}".format("Toni", "Good sir!");
+console.log(result);
